96e696707dd6b122ce2f1c2671cf3d2ca119a030
matan solom <matan.s.dev@gmail.com>
2024-01-04 20:16:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,14 @@
 var express = require('express');
 var app = express();
 
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use("/",express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -23,6 +24,10 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+app.get("/api/timestamp/:date",(req, res)=>{
+  res.json({"erorr":"invalid"})
+})
 
 
 
